bb9812e4b969f2d7c117d49f03c88f0e1e1b6fb4
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-10-29 15:07:28 +0700

    Optimize build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  purge: [
-    "src/**/*.js",
-    "src/**/*.jsx",
-    "src/**/*.ts",
-    "src/**/*.tsx",
-    "public/**/*.html",
-  ],
+  purge: {
+    enabled: true,
+    mode: "layers",
+    layers: ["base", "components", "utilities"],
+    content: ["./src/**/*.ts", "./src/**/*.tsx", "./public/**/*.html"],
+  },
   theme: {
     extend: {},
   },
